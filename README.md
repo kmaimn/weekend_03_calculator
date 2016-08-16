9eec81b99f06fa2763e90eca8b1c0496d926a0fe
kmaimn <kmai.mn@gmail.com>
2016-08-16 13:26:23 -0500

    recommitted files

---
+++ README.md
@@ -0,0 +1,4 @@
+# weekend_03
+##### 15 Aug 2016 - first submission: currently a work in progress. I will be re-doing the assignment this week.
+
+##### 16 Aug 2016 - cleaned up the code in .js, .html files.
---
+++ server/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+var calculations = require('./routes/calculations')
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//define router to calculations file;
+app.use('/calculations', calculations);
+
+//set port for localhost;
+app.set('port', process.env.PORT || 3000);
+
+//catch all get for static files;
+app.get('/*', function (req, res) {
+  var file = req.params[0] || '/views/index.html';
+  res.sendFile(path.join(__dirname, './public', file));
+});
+s
+app.listen(app.get('port'), function () {
+  console.log('Server is running on port: ', app.get('port'));
+});
